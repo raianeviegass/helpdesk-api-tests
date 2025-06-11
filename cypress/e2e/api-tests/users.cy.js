2c189d757f491d92e885c051f27f50f111a83c7a
Raiane Viegas <raianeviegass@gmail.com>
2025-06-11 20:57:04 -0300

    Atualizando formatação

--- cypress/e2e/api-tests/users.cy.js
+++ cypress/e2e/api-tests/users.cy.js
@@ -123,21 +123,21 @@ describe('Testes relacionados a usuários', () => {
         name: usuarioAleatorio.name,
         email: usuarioAleatorio.email,
       };
-    cy.request({
-      method: 'POST',
-      url: urlUsers,
-      failOnStatusCode: false,
-      body: existingUser,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    .then((response) => {
-        expect(response.status).to.eq(409);
-        expect(response.body).to.not.have.property('name');
-        expect(response.body).to.not.have.property('email');
-        expect(response.body).to.have.property('error', 'A user with this name or email already exists.');
-      });
+      cy.request({
+        method: 'POST',
+        url: urlUsers,
+        failOnStatusCode: false,
+        body: existingUser,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          expect(response.status).to.eq(409);
+          expect(response.body).to.not.have.property('name');
+          expect(response.body).to.not.have.property('email');
+          expect(response.body).to.have.property('error', 'A user with this name or email already exists.');
+        });
     });
   });
 
@@ -198,103 +198,103 @@ describe('Testes relacionados a usuários', () => {
     });
   });
 
-    it('Deve retornar 200 e atualizar um usuário existente', () => {
-      cy.request('GET', urlUsers)
-        .then((response) => {
-          const items = response.body;
-          let randomItem = [Math.floor(Math.random() * items.length)];
-          if (randomItem == 0) {
-            randomItem = Number(randomItem) + 1;
-          }
-          cy.request('GET', urlUsers + randomItem)
-            .then((response) => {
-              const specificUserId = response.body.id;
-              const specificUserName = response.body.name;
-              const specificUserEmail = response.body.email;
+  it('Deve retornar 200 e atualizar um usuário existente', () => {
+    cy.request('GET', urlUsers)
+      .then((response) => {
+        const items = response.body;
+        let randomItem = [Math.floor(Math.random() * items.length)];
+        if (randomItem == 0) {
+          randomItem = Number(randomItem) + 1;
+        }
+        cy.request('GET', urlUsers + randomItem)
+          .then((response) => {
+            const specificUserId = response.body.id;
+            const specificUserName = response.body.name;
+            const specificUserEmail = response.body.email;
 
-              const updatedUser = {
-                id: specificUserId,
-                name: 'Nome Atualizado',
-                email: 'emailatualizado@test.com'
-              }
+            const updatedUser = {
+              id: specificUserId,
+              name: 'Nome Atualizado',
+              email: 'emailatualizado@test.com'
+            }
 
-              cy.request({
-                method: 'PUT',
-                url: urlUsers + updatedUser.id,
-                body: updatedUser,
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-              }).then((response) => {
-                expect(response.status).to.eq(200);
-                expect(response.body.user).to.have.property('id', updatedUser.id);
-                expect(response.body.user).to.have.property('name', updatedUser.name);
-                expect(response.body.user.name).to.not.equal(specificUserName);
-                expect(response.body.user).to.have.property('email', updatedUser.email);
-                expect(response.body.user.email).to.not.equal(specificUserEmail);
-                expect(response.body).to.have.property('message', 'User updated successfully.');
-              });
+            cy.request({
+              method: 'PUT',
+              url: urlUsers + updatedUser.id,
+              body: updatedUser,
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            }).then((response) => {
+              expect(response.status).to.eq(200);
+              expect(response.body.user).to.have.property('id', updatedUser.id);
+              expect(response.body.user).to.have.property('name', updatedUser.name);
+              expect(response.body.user.name).to.not.equal(specificUserName);
+              expect(response.body.user).to.have.property('email', updatedUser.email);
+              expect(response.body.user.email).to.not.equal(specificUserEmail);
+              expect(response.body).to.have.property('message', 'User updated successfully.');
             });
-        });
-    });
-
-    it('Deve retornar 404 e não encontrar um usuário para atualizar', () => {
-      cy.request('GET', urlUsers)
-        .then((response) => {
-          const idsExistentes = response.body.map(u => u.id);
-          cy.log(idsExistentes);
-          let idInvalido = Math.max(...idsExistentes) + 1;
-          while (idsExistentes.includes(idInvalido)) {
-            idInvalido++;
-          }
-          cy.request({
-            method: 'GET',
-            url: urlUsers + `${idInvalido}`,
-            failOnStatusCode: false
-          }).then((response) => {
-            expect(response.status).to.eq(404);
-            expect(response.body).to.have.property('error', 'User not found.');
-            cy.log(`ID inexistente: ${idInvalido}`);
           });
-        });
-    });
-
-    it('Deve retornar 200 e excluir um usuário', () => {
-      cy.request('GET', urlUsers)
-        .then((response) => {
-          const items = response.body;
-          let randomItem = [Math.floor(Math.random() * items.length)];
-          if (randomItem == 0) {
-            randomItem = Number(randomItem) + 1;
-          }
-          cy.request('GET', urlUsers + randomItem)
-          cy.request({
-            method: 'DELETE',
-            url: urlUsers + randomItem,
-          }).then((response) => {
-            expect(response.status).to.eq(200);
-          });
-        });
-    });
-
-    it('Deve retornar 404 e não encontrar um usuário para excluir', () => {
-      cy.request('GET', urlUsers)
-        .then((response) => {
-          const idsExistentes = response.body.map(u => u.id);
-          cy.log(idsExistentes);
-          let idInvalido = Math.max(...idsExistentes) + 1;
-          while (idsExistentes.includes(idInvalido)) {
-            idInvalido++;
-          }
-          cy.request({
-            method: 'DELETE',
-            url: urlUsers + `${idInvalido}`,
-            failOnStatusCode: false
-          }).then((response) => {
-            expect(response.status).to.eq(404);
-            expect(response.body).to.have.property('error', 'User not found.');
-            cy.log(`ID inexistente: ${idInvalido}`);
-          });
-        });
-    });
+      });
   });
+
+  it('Deve retornar 404 e não encontrar um usuário para atualizar', () => {
+    cy.request('GET', urlUsers)
+      .then((response) => {
+        const idsExistentes = response.body.map(u => u.id);
+        cy.log(idsExistentes);
+        let idInvalido = Math.max(...idsExistentes) + 1;
+        while (idsExistentes.includes(idInvalido)) {
+          idInvalido++;
+        }
+        cy.request({
+          method: 'GET',
+          url: urlUsers + `${idInvalido}`,
+          failOnStatusCode: false
+        }).then((response) => {
+          expect(response.status).to.eq(404);
+          expect(response.body).to.have.property('error', 'User not found.');
+          cy.log(`ID inexistente: ${idInvalido}`);
+        });
+      });
+  });
+
+  it('Deve retornar 200 e excluir um usuário', () => {
+    cy.request('GET', urlUsers)
+      .then((response) => {
+        const items = response.body;
+        let randomItem = [Math.floor(Math.random() * items.length)];
+        if (randomItem == 0) {
+          randomItem = Number(randomItem) + 1;
+        }
+        cy.request('GET', urlUsers + randomItem)
+        cy.request({
+          method: 'DELETE',
+          url: urlUsers + randomItem,
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+        });
+      });
+  });
+
+  it('Deve retornar 404 e não encontrar um usuário para excluir', () => {
+    cy.request('GET', urlUsers)
+      .then((response) => {
+        const idsExistentes = response.body.map(u => u.id);
+        cy.log(idsExistentes);
+        let idInvalido = Math.max(...idsExistentes) + 1;
+        while (idsExistentes.includes(idInvalido)) {
+          idInvalido++;
+        }
+        cy.request({
+          method: 'DELETE',
+          url: urlUsers + `${idInvalido}`,
+          failOnStatusCode: false
+        }).then((response) => {
+          expect(response.status).to.eq(404);
+          expect(response.body).to.have.property('error', 'User not found.');
+          cy.log(`ID inexistente: ${idInvalido}`);
+        });
+      });
+  });
+});
